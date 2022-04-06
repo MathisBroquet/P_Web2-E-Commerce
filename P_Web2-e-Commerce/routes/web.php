5d3074b40dd7441e81147af5f6fe9dfb801aa1b4
Alexandresamuel King <pd50uyl@etmlnet.local>
2022-04-06 15:44:07 +0200

    menu, système de layout

--- P_Web2-e-Commerce/routes/web.php
+++ P_Web2-e-Commerce/routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/app', function () {
-    return view('app');
+Route::get('/home', function () {
+    return view('pages.home');
+});
+Route::get('/game', function () {
+    return view('pages.game');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
 });
--- P_Web2-e-Commerce/tailwind.config.js
+++ P_Web2-e-Commerce/tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'ored': {
+          DEFAULT: '#F75100',
+          '50': '#FFCAB0',
+          '100': '#FFBC9B',
+          '200': '#FFA172',
+          '300': '#FF854A',
+          '400': '#FF6A21',
+          '500': '#F75100',
+          '600': '#BF3F00',
+          '700': '#872C00',
+          '800': '#4F1A00',
+          '900': '#170700'
+        }
+
+      }
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'amongus': "url('/images/games/amogus.png')"        
+      })
+
+    },
+    plugins: [],
+  }
 }
