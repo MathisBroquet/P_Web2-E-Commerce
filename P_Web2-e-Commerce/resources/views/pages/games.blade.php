b1f8d2b7ad66cb93734a573dca04432d959452b1
Alexandresamuel King <pd50uyl@etmlnet.local>
2022-05-06 15:11:51 +0200

    correction bugs page games

--- P_Web2-e-Commerce/resources/views/pages/games.blade.php
+++ P_Web2-e-Commerce/resources/views/pages/games.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
 
 @section('content')
-<div class="flex">
+<div class="flex border-b-2 border-slate-600 mb-5">
     <div class=" w-1/10">
     </div>
-    <div class="mt-4 h-40 ml w-4/5 flex justify-around items-center text-2xl text-white font-medium">
+    <div class="mt-4 h-40 ml w-4/5 flex justify-around items-center text-2xl text-white font-medium ">
         <div>
             <nav>
                 <ul>
@@ -14,8 +14,8 @@
                             <?php
                             $html = "";
                             for ($i = 0; $i < 4; $i++) {
-                                $html .= "<li class='font-normal'><input type='checkbox' id='scales' name='scales'
-                                ><label for='scales'>Scales</label></li>";
+                                $html .= "<li class='font-normal'><input type='checkbox' id='pc" . $i . "' name='pc" . $i . "'
+                                ><label for='pc" . $i . "'>PC</label></li>";
                             }
                             echo $html;
                             ?>
@@ -32,8 +32,8 @@
                         <?php
                         $html = "";
                         for ($i = 0; $i < 4; $i++) {
-                            $html .= "<li class='font-normal'><input type='checkbox' id='Horreur' name='Horreur'
-                                ><label for='scales'>Horreur</label></li>";
+                            $html .= "<li class='font-normal'><input type='checkbox' id='Horreur" . $i . "' name='Horreur" . $i . "'
+                                ><label for='Horreur" . $i . "'>Horreur</label></li>";
                         }
                         echo $html;
                         ?>
@@ -49,12 +49,12 @@
                         <?php
                         $html = "";
                         for ($i = 0; $i < 4; $i++) {
-                            $html .= "<li class='font-normal'><input type='checkbox' id='ubisoft' name='ubisoft'
-                                ><label for='ubisoft'>Ubisoft</label></li>";
+                            $html .= "<li class='font-normal'><input type='checkbox' id='ubisoft" . $i . "' name='ubisoft" . $i . "'
+                                ><label for='ubisoft" . $i . "'>Ubisoft</label></li>";
                         }
                         echo $html;
                         ?>
-                    </ul>                    
+                    </ul>
                 </li>
             </ul>
         </div>
@@ -70,11 +70,28 @@
     <div class=" w-1/10">
     </div>
 </div>
-<div class="flex">
+<div class="flex mt-5 mb-5">
     <div class=" w-1/10">
     </div>
     <div>
-        
+        <div>
+            Minecraft
+        </div>
+        <div class=" w-fit h-fit ">
+            <img src="/images/games/Minecraft.png" alt="Minecraft">
+        </div>
+
+        <div class="flex bg-white bg-opacity-25 text-gray-400 justify-between h-1/4">
+            <div class="ml-2">
+                Etoile
+            </div>
+            <div>
+                Like
+            </div>
+            <div class="mr-2">
+                Panier
+            </div>
+        </div>
     </div>
     <div class=" w-1/10">
     </div>
