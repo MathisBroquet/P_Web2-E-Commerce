2bf79972479679ecf89be9197fe2800af2cc3dc2
Mathis Broquet <pm17cxg@eduvaud.ch>
2022-05-18 13:08:46 +0200

    Create the branch

--- P_Web2-e-Commerce/resources/views/pages/addGame.blade.php
+++ P_Web2-e-Commerce/resources/views/pages/addGame.blade.php
@@ -20,6 +20,25 @@
 
         <input type="submit" name="test">
     </form>
+
+    <h1>Modify Game</h1>
+    <form action="{{ route('game.modify') }}" method="post">
+        @csrf
+        <input type="text" name="artName" value="" id="" placeholder="name">
+        <input type="text" name="artDescription" value="" id="" placeholder="Description">
+        <input type="text" name="artPathToImage" value="" id="" placeholder="PathToImg">
+        <input type="text" name="artPrice" value="" id="" placeholder="price">
+        <input type="date" name="artReleaseDate" value="" id="" placeholder="release">
+        <input type="integer" name="artFKAuthor" value="" id="" placeholder="fk">
+
+        <select name="pets" id="pet-select">
+            @foreach($authors as $author) 
+                <option value="{{$author->idAuthor}}">{{$author->autCompanyName}}</option>
+            @endforeach
+        </select>
+
+        <input type="submit" name="test">
+    </form>
 </div>
 
 @stop
